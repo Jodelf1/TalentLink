17ab6b2a2ab9a0fccf23f90b5ff3fd0a4e86a385
Jodelfi <jodelfimarimba56@gmail.com>
2024-11-20 14:12:51 +0100

    style: depoimentos

---
+++ assets/js/slider.js
@@ -0,0 +1,28 @@
+const swiper = new Swiper('.slider-wrapper', {
+
+    spaceBetween: 10,
+    grabCursor: true,
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        0:{
+            slidesPerView: 1
+        },
+        620:{
+            slidesPerView: 2
+        },
+        1024:{
+            slidesPerView: 3
+        },   
+    }
+  });
