57c902dd4db48dea36f9fe136fef5783a0d83fc2
Jodelfi <jodelfimarimba56@gmail.com>
2024-12-26 11:06:52 +0100

    feat: adicionei as descrições dos metodos do administrador

--- app/controllers/adminController.php
+++ app/controllers/adminController.php
@@ -1,6 +1,20 @@
 <?php 
 namespace app\controllers;
 
-class adminController{
-    
+class adminController{ 
+    public function index()
+    {
+        controller::view('admin/index');
+    }
+
+    public function createAdmin($data)
+    {
+        //Função para criar um novo admin
+    }
+
+    public function deleteAdmin($admin_id)
+    {
+        //Função para deletar um admin
+    }
+
 }
